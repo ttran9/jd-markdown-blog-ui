6e80761f997597a791b3acbd7fffa6bc36b78072
ttran9 <toddtran9@gmail.com>
2020-08-17 22:25:01 -0700

    adding components and layout.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "semantic-ui-css/semantic.min.css";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { createBrowserHistory } from "history";
+import Layout from "./containers/Layout";
+import PostList from "./containers/PostList";
+import PostDetail from "./containers/PostDetail";
+import PostCreate from "./containers/PostCreate";
+import PostUpdate from "./containers/PostUpdate";
+import PostDelete from "./containers/PostDelete";
+
+// create the history in the browser.
+const history = createBrowserHistory();
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // history is required for proper use of routing.
+    // <Router history={history}>
+    <BrowserRouter>
+      <Layout>
+        <Switch>
+          <Route exact path="/" component={PostList} />
+          <Route path="/create" component={PostCreate} />
+          <Route path="/post/:postSlug" component={PostDetail} />
+          <Route path="/post/:postSlug/update" component={PostUpdate} />
+          <Route path="/post/:postSlug/delete" component={PostDelete} />
+        </Switch>
+      </Layout>
+    </BrowserRouter>
   );
 }
 
