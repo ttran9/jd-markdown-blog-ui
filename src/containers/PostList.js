74e76d9395986a76f97ec90895481452c5336b66
ttran9 <toddtran9@gmail.com>
2020-08-18 23:42:58 -0700

    displaying post content.

--- src/containers/PostList.js
+++ src/containers/PostList.js
@@ -1,8 +1,52 @@
-import React from "react";
-import {Header} from "semantic-ui-react";
+import React, { useEffect, useState } from "react";
+import { Divider, Header, Item } from "semantic-ui-react";
+import axios from "axios";
+import Loader from "../components/Loader";
+import Message from "../components/Message";
 
 const PostList = () => {
-  return <div><Header>Post List</Header></div>;
+  const [posts, setPosts] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      setLoading(true);
+      try {
+        // the response is asynchronous so we can use await (wait for the call from axios)
+        const res = await axios.get("http://127.0.0.1:8000/api/posts/");
+        setPosts(res.data);
+        setLoading(false);
+      } catch (error) {
+        setError(error.message);
+        setLoading(false);
+      }
+    }
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <Header>Post List</Header>
+      <Divider />
+      {error && <Message negative message={error} />}
+      {loading && <Loader />}
+      <Item.Group>
+        {posts?.map((post) => {
+          return (
+            <Item key={post.id}>
+              <Item.Image size="small" src={post.thumbnail} />
+
+              <Item.Content>
+                <Item.Header as="a">{post.title}</Item.Header>
+                <Item.Description>{post.content}</Item.Description>
+              </Item.Content>
+            </Item>
+          );
+        })}
+      </Item.Group>
+    </div>
+  );
 };
 
 export default PostList;
