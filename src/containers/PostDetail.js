b9ca1413ecc2726a1e9ee94d2d534d20628fe053
ttran9 <toddtran9@gmail.com>
2020-08-21 00:35:10 -0700

    removing redundant code.

--- src/containers/PostDetail.js
+++ src/containers/PostDetail.js
@@ -1,47 +1,29 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Container, Header, Image } from "semantic-ui-react";
-import axios from "axios";
 import Loader from "../components/Loader";
 import Message from "../components/Message";
 import { useParams } from "react-router-dom";
 import { api } from "../api";
+import { useFetch } from "../helpers";
 
 const PostDetail = () => {
-  const [post, setPost] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
   // can get params from a hook from react router dom package.
   const { postSlug } = useParams();
-
-  useEffect(() => {
-    async function fetchData() {
-      setLoading(true);
-      try {
-        // the response is asynchronous so we can use await (wait for the call from axios)
-        const res = await axios.get(api.posts.retrieve(postSlug)
-          
-        );
-        console.log(res.data);
-        setPost(res.data);
-        setLoading(false);
-      } catch (error) {
-        setError(error.message);
-        setLoading(false);
-      }
-    }
-    fetchData();
-  }, []);
+  const { data, loading, error } = useFetch(api.posts.retrieve(postSlug));
 
   return (
     <Container text>
       {error && <Message negative message={error} />}
       {loading && <Loader />}
-      {post && (
+      {data && (
         <div>
-          <Image src={post.thumbnail} />
-          <Header as="h1">{post.title}</Header>
-          <Header as="h4">Last Updated: {`${new Date(post.last_updated).toLocaleDateString()}`}</Header>
-          {<p>{post.content}</p>}
+          <Image src={data.thumbnail} />
+          <Header as="h1">{data.title}</Header>
+          <Header as="h4">
+            Last Updated:{" "}
+            {`${new Date(data.last_updated).toLocaleDateString()}`}
+          </Header>
+          {<p>{data.content}</p>}
         </div>
       )}
     </Container>
