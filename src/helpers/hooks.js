8d3dd7e579c4f50b713749c449dc0519aa621750
ttran9 <toddtran9@gmail.com>
2020-08-24 00:44:14 -0700

    using custom axios if the user is logged in.

--- src/helpers/hooks.js
+++ src/helpers/hooks.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
+import { authenticationService, authAxios } from "../services";
 
 function useFetch(url, initialState = null) {
   const [data, setData] = useState(initialState);
@@ -10,8 +11,12 @@ function useFetch(url, initialState = null) {
     async function fetchData() {
       setLoading(true);
       try {
+        let ax = axios;
+        if (authenticationService.isAuthenticated) {
+          ax = authAxios;
+        }
         // the response is asynchronous so we can use await (wait for the call from axios)
-        const res = await axios.get(url);
+        const res = await ax.get(url);
         setData(res.data);
         setLoading(false);
       } catch (error) {
